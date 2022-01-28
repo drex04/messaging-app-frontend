b8b74a999eed63d715916ab162a1601b7fb5b2d8
Drew Nollsch <drew.nollsch@gmail.com>
2022-01-28 10:04:06 +0100

    login with google

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import {
+  getAuth,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import MessageList from './MessageList.js'
+
 
 function App() {
+  let navigate = useNavigate();
+
+  // Authentication functions adapted from GCP Code Lab
+  // https://github.com/firebase/codelab-friendlychat-web
+  // Sign in
+  async function signIn() {
+    // Sign in Firebase using popup auth and Google as the identity provider.
+    var provider = new GoogleAuthProvider();
+    signInWithPopup(getAuth(), provider).then((response) => {
+      console.log(response);
+      localStorage.setItem("Auth Token", response._tokenResponse.refreshToken);
+      localStorage.setItem("User Email", response.user.email);
+    });
+    navigate("/messages");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route
+          path="/"
+          element={<button onClick={signIn}>Log in with Google</button>}
+        />
+        <Route path="/messages" element={<MessageList />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import { initializeApp } from "firebase/app";
+import { getFirebaseConfig } from "./utils/firebase.js";
+
+const firebaseAppConfig = getFirebaseConfig();
+initializeApp(firebaseAppConfig);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
